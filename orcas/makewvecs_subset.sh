f6ede81763a0cc23f6893e15d8883ff1ca0a7dbb
Debasis Ganguly <debforit@gmail.com>
2021-08-17 10:10:20 +0100

    topdocs vocab

--- orcas/makewvecs_subset.sh
+++ orcas/makewvecs_subset.sh
@@ -1,3 +1,3 @@
-cat query_subset.txt | awk '{for (i=1;i<=NF;i++) print $i}'|sort|uniq > vocab.txt
+cat qid_topdoc.txt | awk -F '\t' '{print $2}' | awk '{for (i=1;i<=NF;i++) print $i}'|sort|uniq > vocab.txt
 awk 'FNR==NR{v[$1]=$1; next} {if ($1 in v) print $0}' vocab.txt ~/research/common/wordvecs/glove.6B.300d.txt > glove.subset
 
